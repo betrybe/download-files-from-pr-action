4fb5bad93d4ed4ac1b744bb91cc0a68dce46068c
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-05-26 23:03:55 -0300

    configura parametros para chamada da lista de arquivos modificados no PR

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ const listFiles = async (options) => {
     log
   } = options;
 
-  const { data: files } = await client.pulls.listFiles();
+  const { data: files } = await client.pulls.listFiles({
+    owner,
+    repo,
+    pull_number: prNumber,
+    per_page: 100,
+    page: 1,
+  });
   return files
     .filter(({ filename }) => filename.includes(filterPath))
     .map(({ filename }) => filename);
