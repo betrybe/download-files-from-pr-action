9db2292de99245ffbb7f686d688299f2faf65946
Leandro Parazito <leandroh@gmail.com>
2023-01-09 16:36:55 -0300

    revert to axios

--- index.js
+++ index.js
@@ -6,37 +6,23 @@ const main = require('./main');
 const BATCH_UPDATE_URL = 'https://api.betrybe.dev/content-object-service/external/v1/content_objects/validate_objects'
 
 async function updateContentObjects(files, prNumber, owner, repo, actor) {
-  core.info(`\u001B[34m[INFO] Validating Content Objects modifield on Pull Request ${prNumber}`)
+  core.info(`\u001B[34m[INFO] Updating Content Objects modifield on Pull Request ${prNumber}`)
 
   const repository = `${owner}/${repo}`
   const payload = { files, pr_number: prNumber, repository, github_username: actor }
 
   const basicAuthPassword = process.env.BASIC_AUTH_PASSWORD
   const encodedUsernamePassword = Buffer.from(`squad_cursos:${basicAuthPassword}`).toString('base64')
-  // const headers = {'Authorization': `Basic ${encodedUsernamePassword}`}
-
-  // const options = {
-  //   'Authorization': `Basic ${encodedUsernamePassword}`,
-
-  // }
+  const headers = {'Authorization': `Basic ${encodedUsernamePassword}`}
 
   console.log('payload', payload)
 
-  return await fetch(BATCH_UPDATE_URL, {
-      method: 'POST',
-      headers: new Headers({'Authorization': `Basic ${encodedUsernamePassword}`}),
-      body: JSON.stringify(payload)
-    }).then(async (response) => {
+  return await axios.post(BATCH_UPDATE_URL, payload, { headers })
+    .then(async (response) => {
       core.info('\u001B[34m[INFO] Content Objects updated successfully ✓')
       return { status: response.status, data: response.data }
-    }).catch(async (error) => ({ status: error.response.status, data: error.response.data }))
-
-  // return await axios.post(BATCH_UPDATE_URL, payload, { headers })
-  //   .then(async (response) => {
-  //     core.info('\u001B[34m[INFO] Content Objects updated successfully ✓')
-  //     return { status: response.status, data: response.data }
-  //   })
-  //   .catch(async (error) => ({ status: error.response.status, data: error.response.data }))
+    })
+    .catch(async (error) => ({ status: error.response.status, data: error.response.data }))
 }
 
 async function run() {
